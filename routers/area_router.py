8b3286c5400b387da28a9fe03f8109b398375d14
orangels <orangels0313@163.com>
2024-06-19 16:34:12 +0800

    fix area_router namespce bug

--- routers/area_router.py
+++ routers/area_router.py
@@ -13,14 +13,14 @@ def get_local_service():
 
 @router.post("/api/device/areas", response_model=List[Area],
              status_code=status.HTTP_200_OK)
-def get_all_cameras(camera_service: AreaService = Depends(get_local_service)):
-    return camera_service.get_all_areas()
+def get_all_areas(area_service: AreaService = Depends(get_local_service)):
+    return area_service.get_all_areas()
 
 
 @router.post("/api/device/creatArea", response_model=Area,
              status_code=status.HTTP_201_CREATED)
-def creat_cameras(area: AreaCreat,
-                  area_service: AreaService = Depends(get_local_service)):
+def creat_areas(area: AreaCreat,
+                area_service: AreaService = Depends(get_local_service)):
     # TODO MQTT client
     return area_service.create_area(area)
 
@@ -28,27 +28,28 @@ def creat_cameras(area: AreaCreat,
 @router.post("/api/device/deleteCameraAreas",
              status_code=status.HTTP_204_NO_CONTENT)
 def delete_area_by_camera_id(camera_id: int,
-                             area_service: AreaService = Depends(get_local_service)):
+                             area_service: AreaService = Depends(
+                                 get_local_service)):
     area_service.delete_area_by_camera_id(camera_id)
     # TODO MQTT client
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+
 @router.post("/api/device/deleteArea",
              status_code=status.HTTP_204_NO_CONTENT)
 def delete_area_by_id(area_id: int,
-                    area_service: AreaService = Depends(get_local_service)):
+                      area_service: AreaService = Depends(get_local_service)):
     area_service.delete_area(area_id)
     # TODO MQTT client
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@router.post("/api/device/updateArea", response_model=Area, status_code=status.HTTP_200_OK)
-def update_area_by_id(area_id: int, area_update: AreaUpdate, area_service: AreaService = Depends(get_local_service)):
+@router.post("/api/device/updateArea", response_model=Area,
+             status_code=status.HTTP_200_OK)
+def update_area_by_id(area_id: int, area_update: AreaUpdate,
+                      area_service: AreaService = Depends(get_local_service)):
     updated_area = area_service.update_area(area_id, area_update)
     if updated_area is None:
         raise HTTPException(status_code=404, detail="User not found")
     # TODO MQTT client
     return updated_area
-
-
-
